292297735f9807a56c8b5c5c74478e656c37416d
marcianomatias <marcianomatias1@hotmail.com>
2024-10-05 01:00:18 -0300

    inicio do curso

---
+++ aula_1.py
@@ -0,0 +1,15 @@
+# Comentários
+print(123) # Comentários
+
+##
+# Multi linhas
+#
+#
+# ##
+"""
+escrever Doscting
+é lido pelo python
+"""
+'''
+Escrever notas tbm Docstrings
+'''
---
+++ aula_2.py
@@ -0,0 +1,7 @@
+# Função print recebe  argumentos e printa na tela
+# quebra de linha \r \n => CRLF
+print(12, 34, sep=',', end='\n\r')
+print(56, 78, sep='/')
+print(56, 78, sep="-")
+print(56, 78, sep="_")
+print(78, 98)
